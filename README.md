d964ca435997ce642be5b6d74a3efa3a22749b21
infoTercen <info@tercen.com>
2019-11-13 20:41:05 +0000

    init the repo

---
+++ README.md
@@ -0,0 +1,38 @@
+# neighbourhood operator
+
+##### Description
+`neighbourhood` operator returns a neighbourhood enrichment of clusters of tissue cells. 
+
+##### Usage
+
+Input projection|.
+---|---
+`y-axis`        | numeric, input data, object numbers (from cell profiler for example)
+`x-axis`        | numeric, input data, object numbers of neighbours (from cell profiler for example)
+`color`         | character, input data, color by cluster label
+
+Input Parameter |.
+---|---
+`permutation`   | numeric, number of permutations
+`num of cores`  | numeric, number of cores
+`seed`          | numeric, seed setting, -1 indicates random
+
+Output relations|.
+---|---
+`p`             | numeric, p-value of enrichment for each cluster pair
+
+##### Details
+
+The operator takes as input the neighboorhood annotation of cells (i.e. object) aswell as the cluster label of the cells. The ouput is a p-value indicating how enriched (or depleted), in terms of neighbours, for each cluster to cluster relationship.
+It uses the `neighbouRhood` code on the bodenmiller github location (https://github.com/BodenmillerGroup/neighbouRhood).
+
+The neighbour relationship of the cells typically comes from Cell Profiler tool but any tool which generates cell neighbourhood information can be used. 
+
+#### References
+
+
+##### See Also
+
+[clusterx_operator](https://github.com/tercen/clusterx_operator)
+
+#### Examples
---
+++ main.R
@@ -0,0 +1,55 @@
+library(tercen)
+library(tidyverse)
+library(neighbouRhood)
+library(data.table)
+library(parallel)
+
+
+# initialize
+seed = as.integer(ctx$op.value('seed'))
+if (seed>0) set.seed(seed)
+
+n_perm = as.integer(ctx$op.value('n_perm'))
+ncores = as.integer(ctx$op.value('ncores'))
+
+ctx = tercenCtx()
+
+color_name <- unlist(ctx$colors)
+data <- ctx$select(c(".x",".y", color_name))
+
+objects <-
+  data %>% 
+  select(-.x) %>% 
+  distinct(.y, .keep_all = TRUE) %>% 
+  rename("ObjectNumber" =".y", "label" = color_name) %>% 
+  mutate(ObjectID = ObjectNumber, ObjectName = "cell", ImageNumber = 1, group = 1 ) %>%
+  as.data.table()
+  
+objects_relationship <-
+  data %>%
+  select(-color_name) %>% 
+  mutate("First Object ID" = .y, "Second Object ID" = .x, group=1, ct =1) %>%
+  select(-.x, -.y) %>% 
+  as.data.table()
+
+
+dat_baseline = apply_labels(objects, objects_relationship) %>%
+  aggregate_histo()
+
+
+dat_perm = rbindlist(mclapply(1:n_perm, function(x){
+  dat_labels = shuffle_labels(objects)
+  apply_labels(dat_labels, objects_relationship) %>%
+    aggregate_histo()
+},mc.cores = ncores
+), idcol = 'run')
+
+dat_p <- calc_p_vals(dat_baseline, dat_perm, n_perm = 1000, p_tresh = 0.01)
+
+result <- dat_p %>% 
+  mutate(sig = as.numeric(sig), direction = as.numeric(direction))  %>%
+  select(p, FirstLabel, SecondLabel)  %>% 
+  mutate(.ci = 0, .ri = 0)  %>% 
+  ctx$addNamespace()
+
+ctx$save(result)
